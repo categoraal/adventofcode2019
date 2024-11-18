df2ffbdb4597efe958dc41282492a95c20469dfe
categoraal <76538493+categoraal@users.noreply.github.com>
2024-11-18 13:30:20 +0100

    much faster implementation of day 19 part 2

--- day19.py
+++ day19.py
@@ -7,7 +7,6 @@ def r_ins():
     for i,v in enumerate(data): ins[i] = v
     return ins
 
-
 size = 50
 kaart = {}
 cnt = 0
@@ -22,32 +21,48 @@ for y in range(size):
 
 print(cnt)
 
-
-origin = [6+9,5+8]
-queue = deque([tuple(origin)])
-
-dirs = ((1,0),(0,1))
-offset = ((-99,99),(99,-99))
-kaart = {tuple(origin):1}
-flag = False
+dirs = ((0,1),(1,0),(1,1))
+pos = (6,5)
 while True:
-    p = queue.popleft()
-    if flag:
+    x,y = pos
+    ins = r_ins()
+    if y-99 > 0 and computer(ins,[x+99,y-99],direct=True)[0] == 1: 
+        print(x*10000+y-99)
         break
-    x,y = p
     for dx,dy in dirs:
+        ins = r_ins()
         nx,ny = x+dx,y+dy
-        if (nx,ny) not in queue and (nx,ny) not in kaart:
-            ins = r_ins() 
-            r = computer(ins,[nx,ny],direct=True)[0]
-            kaart[(nx,ny)] = r
-            if r == 1:
-                queue.append((nx,ny))
-                for ox,oy in offset:
-                    x2,y2 = nx+ox,ny+oy   
-                    if (x2,y2) in kaart and kaart[(x2,y2)] == 1:
-                        if ox == -99:
-                            print((nx+ox)+10000+ny)
-                        else:
-                            print(nx*10000+ny+oy)
-                        flag = True 
+        r = computer(ins,[nx,ny],direct=True)[0]
+        if r == 1:
+            pos = (nx,ny)
+            break
+
+
+# origin = [6+9,5+8]
+# queue = deque([tuple(origin)])
+
+# dirs = ((1,0),(0,1))
+# offset = ((-99,99),(99,-99))
+# kaart = {tuple(origin):1}
+# flag = False
+# while True:
+#     p = queue.popleft()
+#     if flag:
+#         break
+#     x,y = p
+#     for dx,dy in dirs:
+#         nx,ny = x+dx,y+dy
+#         if (nx,ny) not in queue and (nx,ny) not in kaart:
+#             ins = r_ins() 
+#             r = computer(ins,[nx,ny],direct=True)[0]
+#             kaart[(nx,ny)] = r
+#             if r == 1:
+#                 queue.append((nx,ny))
+#                 for ox,oy in offset:
+#                     x2,y2 = nx+ox,ny+oy   
+#                     if (x2,y2) in kaart and kaart[(x2,y2)] == 1:
+#                         if ox == -99:
+#                             print((nx+ox)+10000+ny)
+#                         else:
+#                             print(nx*10000+ny+oy)
+#                         flag = True 
